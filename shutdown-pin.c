a75315ead098d9685abf0143cf3dae3f03caf1a7
nodebug666 <weedbug666@gmail.com>
2018-05-11 13:35:13 +0800

    Enable daemon mode

--- shutdown-pin.c
+++ shutdown-pin.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 #include <strings.h>
 #include <errno.h>
+#include <unistd.h>
 #include <sys/reboot.h>
 #include <sys/types.h>
+#include <time.h>
 #include "GPIO.h"
 
+#define LOG_FILE     "/var/log/shutdown-pin"
 #define DEFAULT_PIN  21
 #define SLEEP_PERIOD 1
 #define ALTER_REPEAT 10
 
 volatile int* gpio = NULL;                   
+int nodaemon = 0;                   //Don't run this program as daemon
 int falarm = 0;                     //False alarm
 int persist = 1;                    //Persist GPIO mode
 int spin = DEFAULT_PIN;             //specified pin
@@ -35,6 +39,8 @@ int programFlags(int args,char** argv){
             persist = 0;
         else if(strcasecmp(argv[i],"--quiet") == 0)
             fclose(stdout);
+        else if(strcasecmp(argv[i],"--no-daemon") == 0)
+            nodaemon = 1;
         else if(strcasecmp(argv[i],"--pin") == 0 && args > ++i){
             if((spin = atoi(argv[i])) == -1 ){
                 printf("Unknow specified pin %s\n",argv[i]);
@@ -54,6 +60,15 @@ int getTrigger(int pin){
         case PULL_RESISTOR_DOWN:    return 1;
         default:                    return -1;
     }
+}
+
+void gtime(char* buffer,size_t size){
+    time_t cur_time;
+    struct tm * time_info;
+    
+    time(&cur_time);
+    time_info = localtime(&cur_time);
+    strftime(buffer, size, "%H:%M:%S", time_info);
 }
 
 void setGPIO(int pin){
@@ -79,16 +94,22 @@ int main(int args, char** argv){
         printf("Can not mapping process, please check for permission. errno code=%d\n",errno);
         return -3;
     }
-    
     printf("using pin %d, give %s volt to pin %d, and wait for %d second to trigger shutdown\n",
             spin,
             (getTrigger(spin)) ? "HIGH" : "LOW",
             spin,
             SLEEP_PERIOD * ALTER_REPEAT
     );
-    
+    if(nodaemon == 0 && daemon(0,1) == -1){
+        printf("Failed to become daemon, errno code = %d\n",errno);
+        return -5;
+    }
+    if(freopen(LOG_FILE,"a+",stdout) == NULL){
+        printf("Failed to open log file %s, errno code = %d\n",LOG_FILE,errno);
+        return -6;
+    }
     setGPIO(spin);
- 
+    
     int alter = ALTER_REPEAT;
     while(1){
 
@@ -98,13 +119,17 @@ int main(int args, char** argv){
         int trigger_on = (READ(spin) == getTrigger(spin));
         alter = (trigger_on) ? (alter-1) : (ALTER_REPEAT);
 
-        if(alter <= 0){
-            if(falarm)
-                printf("False alarm!\n");
-            else
+        if(alter == 0){
+            char str[256];
+            gtime(str,sizeof(str));
+            printf("[%s]%s\n",str, (!falarm) ? "Shutdown" : "False alarm!");
+            fflush(stdout);
+
+            if(!falarm)
                 sync(),reboot(RB_POWER_OFF);
         }
 
         sleep(SLEEP_PERIOD);
     }
 }
+
